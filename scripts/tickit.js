4a88bf5afb275196595d115bc737cb780b522711
Anik98B <anikbhowmik0188@gmail.com>
2024-02-19 03:57:27 +0600

    added seat select section

--- scripts/tickit.js
+++ scripts/tickit.js
@@ -1,18 +1,39 @@
-function handlerButton(){
-    const select= document.getElementById('seat-select');
-    select.scrollIntoView({behavior:'smooth'})
+function handlerButton() {
+    const select = document.getElementById('seat-select');
+    select.scrollIntoView({ behavior: 'smooth' })
 }
 
-//  coupon input ---
-const couponButton= document.getElementById('coupon-button');
-couponButton.addEventListener('click', function(){
-    let couponInput= document.getElementById('coupon-input').value.toUpperCase();
-
-    if(couponInput==='NEW15' || couponInput=== 'Couple 20'){
-        document.getElementById(couponInput).classList.add('hidden');
-        couponButton.classList.add('hidden');
+document.addEventListener('DOMContentLoaded', function () {
+    const seats = document.querySelectorAll('.seat');
+    const seatsCount = document.getElementById('seat-count');
+    const availableSeats = document.getElementById('available-seats');
+    const SelectedList = document.getElementById('Selected-list');
+    const seatPrice = 550;
+    let selectedSeats = 0;
+    let availableSeat = 16;
+    seats.forEach(seat => {
+        seat.addEventListener('click', function () {
+            if (seat.classList.contains('bg-green-500')) {
+                seat.classList.remove('bg-green-500');
+                selectedSeats--;
+                availableSeat++;
+            } else {
+                seat.classList.add('bg-green-500');
+                selectedSeats++;
+                availableSeat--;
+            }
+            seatsCount.textContent = `${selectedSeats}`;
+            availableSeats.textContent = `${availableSeat}`;
+            updateSelectedSeatsList()
+        })
+    })
+    function updateSelectedSeatsList() {
+        SelectedList.innerHTML = '';
+        selectedSeats.forEach(seat => {
+            const listItem = document.createElement('li');
+            listItem.textContent = `Seat: ${seat} - Economy - Price: $${seatPrice}`;
+            SelectedList.appendChild(listItem);
+        });
     }
-    else{
-        alart('Incorret coupon code');
-    }
+    
 })
